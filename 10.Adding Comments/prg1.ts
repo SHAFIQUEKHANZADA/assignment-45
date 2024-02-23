1fcff78fca8c163dd01442e34893c07dd6c8dd56
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-02-23 17:01:55 +0500

    Add files via upload

---
+++ 10.Adding Comments/prg1.ts
@@ -0,0 +1,5 @@
+//Exercise 10
+//MY name is Shafique Ur Rehman
+//Dated :23/02/2024
+//this program will run simple code
+console.log("Hello friends!");
---
+++ 10.Adding Comments/prg2.js
@@ -0,0 +1,5 @@
+//Exercise 10
+//MY name is Shafique Ur Rehman
+//Dated :23/02/2024
+//this program will multiplication
+console.log(5 * 2);
