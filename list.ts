eda0ca42ae0e1dfc0da67498681e4384e1df71bb
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-02-24 11:19:26 +0500

    Add files via upload

---
+++ list.ts
@@ -0,0 +1,5 @@
+let guest_list: string= ["Zayan", "Tanzeel", "Aoun"];
+
+for(let i=0; i<guest_list.length; i++) {
+    console.log(`Dear ${guest_list},\n\nYou are cordially invited to dinner. It would be an honor to have your company.\n\nThank you`);
+}
