20e9fc77bc8e918b1f92eff014bdb1039bd8c2f3
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-06 12:18:44 +0500

    Add files via upload

---
+++ Exercise no.13/car.js
@@ -0,0 +1,5 @@
+var favorateCar = ["Civic", "Grande", "lamorghini", "ferrari"];
+for (var _i = 0, favorateCar_1 = favorateCar; _i < favorateCar_1.length; _i++) {
+    var cars = favorateCar_1[_i];
+    console.log("I would like to own a ".concat(cars));
+}
---
+++ Exercise no.13/car.ts
@@ -0,0 +1,5 @@
+let favorateCar: string[] = ["Civic", "Grande", "lamorghini", "ferrari"];
+
+for (let cars of favorateCar) {
+    console.log(`I would like to own a ${cars}`)
+}
