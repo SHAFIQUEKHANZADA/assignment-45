29ef89c5efaf7b1755bcc299bb18ecbda2c85881
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-06 12:15:39 +0500

    Add files via upload

---
+++ Exercise no.11/app.js
@@ -0,0 +1,7 @@
+"use strict";
+//by using for...of loop
+let names = ["zayan", "talha", "hamdan", "kumail"];
+console.log("List of my friends:");
+for (let name of names) {
+    console.log(name);
+}
---
+++ Exercise no.11/app.ts
@@ -0,0 +1,8 @@
+//by using for...of loop
+
+let names: string[] = ["zayan", "talha", "hamdan", "kumail"]
+console.log("List of my friends:")
+
+for (let name of names) {
+   console.log(name)
+}
