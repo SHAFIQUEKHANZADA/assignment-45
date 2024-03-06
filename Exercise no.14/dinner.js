415679924e91506d316458f2b7f00cd45c511f31
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-06 12:19:51 +0500

    Add files via upload

---
+++ Exercise no.14/dinner.js
@@ -0,0 +1,5 @@
+var guest_list = ["Zayan", "Tanzeel", "Aoun"];
+for (var _i = 0, guest_list_1 = guest_list; _i < guest_list_1.length; _i++) {
+    var list = guest_list_1[_i];
+    console.log("".concat(list, ", \nYou are cordially invited to dinner.\t \nThank you. \t"));
+}
---
+++ Exercise no.14/dinner.ts
@@ -0,0 +1,5 @@
+let guest_list: string[]= ["Zayan", "Tanzeel", "Aoun"];
+
+for (let list of guest_list) {
+    console.log(`${list}, \nYou are cordially invited to dinner.\t \nThank you. \t`)
+}
