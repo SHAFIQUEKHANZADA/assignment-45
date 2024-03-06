62bd49329f09d909625c9c315cdc10a302c6602d
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-06 12:17:35 +0500

    Add files via upload

---
+++ Exercise no.12/gr.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ["zayan", "talha", "hamdan", "kumail"];
+let message = "Monday ko governor house jana hy.";
+for (let name of names) {
+    console.log(`${name}, ${message}`);
+}
---
+++ Exercise no.12/gr.ts
@@ -0,0 +1,6 @@
+let names: string[] = ["zayan", "talha", "hamdan", "kumail"]
+let message: string = "Monday ko governor house jana hy."
+
+for (let name of names) {
+   console.log(`${name}, ${message}`)
+}
