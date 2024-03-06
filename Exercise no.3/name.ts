c2cd64e920186a0f7adcb574eb555fd25b0bf655
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-06 12:26:50 +0500

    Update name.ts

--- Exercise no.3/name.ts
+++ Exercise no.3/name.ts
@@ -7,4 +7,5 @@ console.log(personName.toLowerCase());
 console.log(personName.toUpperCase());
 
 //TitleCase
-console.log(personName.replace(/\bw/g,c => c.toUpperCase()));
+console.log(personName.charAt(0).toUpperCase() + 
+personName.slice(1).toLowerCase());
