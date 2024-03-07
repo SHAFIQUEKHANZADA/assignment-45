6a34cba9cb370f447ae165d9a200524d8a01e748
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-07 10:23:14 +0500

    Add files via upload

---
+++ Exercise no 22/error.ts
@@ -0,0 +1,8 @@
+let array: string[] = ["Talha", "Zayan", "Hamdan", "Tanzeel", "Sandeed"];
+
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[2]);
+// console.log(array[7]);
+console.log(array[0]);
+console.log(array[1]);
