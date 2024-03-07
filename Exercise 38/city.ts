6a670ec21d55685722bbe1dee307d82cc38d75b2
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-07 13:56:26 +0500

    Add files via upload

---
+++ Exercise 38/city.ts
@@ -0,0 +1,6 @@
+function describe_city(city: string = "Karachi", country: string  = "Pakistan"): void {
+   console.log(`${city} is in ${country}`)
+}
+describe_city()
+describe_city("Queta")
+describe_city("Dehli", "India")
