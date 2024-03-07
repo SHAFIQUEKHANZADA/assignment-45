a39c1c9ec79fa49aecfe454d960b8e6f7f371651
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-07 13:57:39 +0500

    Add files via upload

---
+++ Exercise 39/num.js
@@ -0,0 +1,6 @@
+function city_country(city, country) {
+    console.log("\"".concat(city, "\", \"").concat(country, "\""));
+}
+city_country("Islamabad", "Pakistan");
+city_country("Toranto", "Canada");
+city_country("Moscow", "Russia");
---
+++ Exercise 39/num.ts
@@ -0,0 +1,6 @@
+function city_country(city: string, country: string): void {
+  console.log(`"${city}", "${country}"`)
+}
+city_country("Islamabad", "Pakistan")
+city_country("Toranto", "Canada")
+city_country("Moscow", "Russia")
