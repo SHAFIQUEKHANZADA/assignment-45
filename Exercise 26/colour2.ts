2f0ad48df81ae1e64c85232206221af55a308dbb
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-07 11:25:12 +0500

    Add files via upload

---
+++ Exercise 26/colour2.ts
@@ -0,0 +1,17 @@
+
+//version with if block
+let alien_color: string = 'green';
+
+// If-else chain to determine points based on alien's color
+if (alien_color === 'green') {
+  console.log("Player just earned 5 points for shooting the green alien!");
+} else {
+  console.log("Player just earned 10 points!");
+}
+
+//version with else block
+if (alien_color !== 'green') {
+  console.log("Player just earned 5 points for shooting the green alien!");
+} else {
+  console.log("Player just earned 10 points!");
+}
