0b16ab971d13c74bf198d6978ac48e9ca6caa02d
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-07 10:08:51 +0500

    Add files via upload

---
+++ Exercise no.19/dinner.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var dinner_1 = require("../Task no.14(Guest List)/dinner");
+console.log("".concat(dinner_1.guest_list, ", people you are inviting to dinner."));
---
+++ Exercise no.19/dinner.ts
@@ -0,0 +1,2 @@
+import{ guest_list } from '../Task no.14(Guest List)/dinner'
+console.log(`${guest_list}, people you are inviting to dinner.`)
