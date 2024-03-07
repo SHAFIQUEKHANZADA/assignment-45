2bc550071aa6f8cd8b0ff1bf09a62078e6c87451
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-07 10:47:26 +0500

    Add files via upload

---
+++ Exercise 23/test.js
@@ -0,0 +1,22 @@
+var car = 'subaru';
+// Test 1
+console.log("Test 1: Is car equal to 'subaru'? I predict True.");
+console.log(car === 'subaru');
+// Test 2
+console.log("Test 2: Is car not equal to 'honda'? I predict True.");
+console.log(car !== 'honda');
+// Test 3
+console.log("Test 3: Does car have a length greater than 4? I predict True.");
+console.log(car.length > 4);
+// Test 4
+console.log("Test 4: Is car equal to 'Subaru' (case-sensitive comparison)? I predict False.");
+console.log(car === 'Subaru');
+// Test 5
+console.log("Test 5: Is car equal to 'subaru' or 'honda'? I predict True.");
+console.log(car === 'subaru' || car === 'honda');
+// Test 6
+console.log("Test 6: Is car not equal to 'subaru'? I predict false.");
+console.log(car !== 'subaru');
+// Test 7
+console.log("Test 7: Is car lenth is greater than 9 ? I predict false.");
+console.log(car.length > 9);
