fe66060d3e344b1c49500dc339cefa39d3fc5f73
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-15 11:56:02 +0500

    Add files via upload

---
+++ Exercise 41/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mag_name = void 0;
+var mag_name = ["Harry Houdini", "Derren Brown", "Dynamo"];
+exports.mag_name = mag_name;
+function see_mag(magicin) {
+    mag_name.forEach(function (mag_name) {
+        console.log(mag_name);
+    });
+}
+see_mag(mag_name);
