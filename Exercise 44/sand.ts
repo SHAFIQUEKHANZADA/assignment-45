81a4d7088174547aed396f3f90ce35ab5338cc05
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-19 10:00:06 +0500

    Add files via upload

---
+++ Exercise 44/sand.ts
@@ -0,0 +1,6 @@
+function make_sandwich(...item: string[]) {
+    console.log(`Making a sandwich with: ${item.join( ', ')}.`);   
+}
+make_sandwich("ham", "cheese")
+make_sandwich("deli meat", "lettuce", "tomato")
+make_sandwich("mayonnaise", "mustard", "Olives", "Avocado")
